7cb8d1c80667fc2187149c9fa1c5da19fe6e3535
Sergey <barclays1313@gmail.com>
2021-11-05 23:15:10 +0300

    feat: implement 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs'),
+  path = require('path'),
+  { stdout, stdin } = require('process');
+
+const pathText = path.join(`${__dirname}/text.txt`);
+const writeableStream = fs.createWriteStream(pathText);
+
+stdout.write('Hello everyone, enter your message...\n');
+stdin.on('data', data => {
+  if (data.toString() == 'exit') process.exit();
+  writeableStream.write(data);
+});
+
+process.on('SIGINT', () => process.exit());
+process.on('exit', () => stdout.write('Bye!'));
